91467e54956eba13316da484cfa5446e8c28df37
zigzagbob <zigzagbob@qq.com>
2018-08-20 14:16:55 +0800

    pre release gs code
    2018.8.20 14:16

--- Gluttonous/main.cpp
+++ Gluttonous/main.cpp
@@ -3,7 +3,9 @@
 #include <QtWidgets/QApplication>
 #include <QtPlugin>
 
-//Q_IMPORT_PLUGIN(QWindowsIntegrationPlugin);
+#ifdef QT_NO_DEBUG
+Q_IMPORT_PLUGIN(QWindowsIntegrationPlugin);
+#endif
 
 int main(int argc, char *argv[])
 {
